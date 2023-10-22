4f26c3114220f5d2fdc9d307c9f1dc090d196965
Chopper <66927724+Drian-key@users.noreply.github.com>
2023-10-22 19:39:45 +0700

    add: README.md, index.html, index.js, test.js (#3)

---
+++ 2. Run NodeJS File/README.md
@@ -0,0 +1,8 @@
+## Open your Gitbash and run your js file
+
+go to file directory
+example cd ./2. Run NodeJS File
+
+```bash
+  node yourname.js
+```
---
+++ 2. Run NodeJS File/test.js
@@ -0,0 +1,7 @@
+console.log("Hello Drian");
+
+const printName = (name) => `Hi, my name is ${name}`;
+
+console.log(printName("Drian from test js"));
+
+module.exports = printName;
